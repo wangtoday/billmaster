27f233840cf5e8b5988d16b83f6ce89edf2e81cc
周旺 <zhouwang@phoenixtv.com>
2020-10-20 17:38:10 +0800

    Initial Commit

---
+++ cloudfunctions/sum/index.js
@@ -0,0 +1,17 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  console.log(wxContext)
+  return {
+    event,
+    openid: wxContext.OPENID,
+    appid: wxContext.APPID,
+    unionid: wxContext.UNIONID,
+    name: '狗儿子思思简单不得了'
+  }
+}
