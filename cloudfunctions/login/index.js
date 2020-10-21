8278136af4bcedb5a44e9544e6cae1211ec41672
周旺 <zhouwang@phoenixtv.com>
2020-10-21 11:51:58 +0800

    refactor: 删除无关的 console log 信息

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -1,6 +1,7 @@
 // 云函数模板
 // 部署：在 cloud-functions/login 文件夹右击选择 “上传并部署”
-
+// 有关 auth 的信息, 只能再这部分的 cloud 拿到, 
+// 然后返回给前端的小程序
 const cloud = require('wx-server-sdk')
 
 // 初始化 cloud
@@ -16,10 +17,6 @@ cloud.init({
  * 
  */
 exports.main = async (event, context) => {
-  console.log(event)
-  console.log(context)
-  console.log('嗷嗷嗷, 来这里了:::')
-
   // 可执行其他自定义逻辑
   // console.log 的内容可以在云开发云函数调用日志查看
 
